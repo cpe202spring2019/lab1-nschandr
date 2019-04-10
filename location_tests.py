1bee78fbb90eb8402a149766bd8818cbb598bb31
Nithya Shamini Chandran <nithyachandran@Nithyas-MacBook-Pro.local>
2019-04-10 09:34:03 -0700

    I made these changes

--- location_tests.py
+++ location_tests.py
@@ -4,8 +4,24 @@ from location import *
 class TestLab1(unittest.TestCase):
 
     def test_repr(self):
-        loc = Location("SLO", 35.3, -120.7)
-        self.assertEqual(repr(loc),"Location('SLO', 35.3, -120.7)")
+        loc1 = Location("SLO", 35.3, -120.7)
+        self.assertEqual(repr(loc1),"Location('SLO', 35.3, -120.7)")
+        loc2 = Location("Paris", 48.9, 2.4)
+        self.assertEqual(repr(loc2),"Location('Paris', 48.9, 2.4)")
+
+    def test_eq(self):
+        loc1 = Location("SLO", 35.3, -120.7)
+        loc2 = Location("Paris", 48.9, 2.4)
+        self.assertNotEqual(loc1,loc2)
+        loc3 = loc1
+        self.assertEqual(loc1, loc3)
+
+    def test_init(self):
+        loc1 = Location("SLO", 35.3, -120.7)
+        self.assertEqual(loc1.name, 'SLO')
+        self.assertEqual(loc1.lat, 35.3)
+        self.assertEqual(loc1.lon, -120.7)
+
     
     # Add more tests!
 
